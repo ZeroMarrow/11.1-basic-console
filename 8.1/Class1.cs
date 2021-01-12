98ff555ab38d196c7b6a75a9ffe5027489ebabc6
ИСТОРИН ВЛАД <xloksik3@gmail.com>
2021-01-12 13:07:03 +0200

    Добавьте файлы проекта.

---
+++ 8.1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._1
+{
+    class Class1
+    {
+        private double cal;
+        private double mas;
+
+        public Class1(double cal, double mas)
+        {
+            this.cal = cal;
+            this.mas = mas;
+        }
+        private double Cmass()
+        {
+            return mas * cal / 100;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Калорийность продукта:" + Cmass());
+        }
+    }
+}
---
+++ 8.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите колорийность продукта на 100г веса: ");
+            double c = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите вес продукта в граммах: ");
+            double m = double.Parse(Console.ReadLine());
+            Class1 class1 = new Class1(c,m);
+            class1.Print();
+            Console.ReadKey();
+        }
+    }
+}
